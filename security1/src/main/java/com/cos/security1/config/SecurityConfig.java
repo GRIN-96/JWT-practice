359d3775bd51e421c6a1d4f3ae967e501750b72d
carlos <bnmnil96@gmail.com>
2023-03-10 07:56:22 +0900

    설명 추

--- security1/src/main/java/com/cos/security1/config/SecurityConfig.java
+++ security1/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{  // 스프링 
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
 		http.authorizeRequests()
-			.antMatchers("/user/**").authenticated()
+			.antMatchers("/user/**").authenticated()  // 인증만 되면 들어갈 수 있는 주소 !!!!! 
 			.antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')") 
 			.antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')") // 권한이 있는사람에게만 접근을부여한다.
 			.anyRequest().permitAll()  // 나머지 모든 요청은 접근을 허용한다
